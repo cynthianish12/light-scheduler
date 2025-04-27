cd40c178c47ee204de7c438ad2a6208004de98b2
cynthianish12 <cynthianish12@gmail.com>
2025-04-27 14:26:32 +0200

    second

---
+++ arduino-codes.cpp
@@ -0,0 +1,22 @@
+// Arduino code (should be uploaded separately)
+const int relayPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW); // Start with light off
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    
+    if (command == '1') {
+      digitalWrite(relayPin, HIGH); // Turn light on
+      Serial.println("Light ON");
+    } else if (command == '0') {
+      digitalWrite(relayPin, LOW); // Turn light off
+      Serial.println("Light OFF");
+    }
+  }
+}
